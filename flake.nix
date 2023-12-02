5e36b6f6f1f8c5d5f844408a2b658900c1628d79
Micah N Gorrell <m@minego.net>
2023-12-02 16:22:45 -0700

    Allow building on arm as well

--- flake.nix
+++ flake.nix
@@ -1,24 +1,20 @@
 {
-	description			= "A flake for building my swapmod plugin for interception-tools";
+	description			= "A flake for building my swapmods plugin for interception-tools";
 	inputs.nixpkgs.url	= "github:NixOS/nixpkgs/nixos-unstable";
 
-	outputs = { self, nixpkgs }: {
+	outputs = { self, nixpkgs, ... }:
+	{
 		overlay = self: super: {
 			minego = (super.minego or {}) // {
-				swapmod = super.pkgs.callPackage ./derivation.nix { };
+				swapmods = super.pkgs.callPackage ./derivation.nix { };
 			};
 		};
 
-		packages = {
-			"x86_64-linux" = rec {
-				swapmod = nixpkgs.legacyPackages."x86_64-linux".callPackage ./derivation.nix { };
-				default = swapmod;
-			};
-			"aarch64-linux" = rec {
-				swapmod = nixpkgs.legacyPackages."aarch64-linux".callPackage ./derivation.nix { };
-				default = swapmod;
-			};
-		};
+		packages.x86_64-linux.swapmods = nixpkgs.legacyPackages.x86_64-linux.callPackage ./derivation.nix {};
+		packages.x86_64-linux.default = self.packages.x86_64-linux.swapmods;
+
+		packages.aarch64-linux.swapmods = nixpkgs.legacyPackages.aarch64-linux.callPackage ./derivation.nix {};
+		packages.aarch64-linux.default = self.packages.aarch64-linux.swapmods;
 	};
 }
 
