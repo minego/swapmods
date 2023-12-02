0574aaa69472540a4dad4258a104f1ee49909cdb
Micah N Gorrell <m@minego.net>
2023-12-02 15:56:20 -0700

    Allow building on arm as well

--- flake.nix
+++ flake.nix
@@ -1,15 +1,25 @@
 {
-	description = "A flake for building my swapmods plugin for interception-tools";
+	description			= "A flake for building my swapmods plugin for interception-tools";
 
-	outputs = { self, nixpkgs }: {
+	inputs = {
+		nixpkgs.url		= "github:NixOS/nixpkgs/nixos-unstable";
+		flake-utils.url	= "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ]  (system:
+		let pkgs = nixpkgs.legacyPackages.${system}; in
+	{
+		packages = rec {
+			swapmods = pkgs.callPackage ./derivation.nix { };
+			default = swapmods;
+		};
+
 		overlay = self: super: {
 			minego = (super.minego or {}) // {
 				swapmods = super.pkgs.callPackage ./derivation.nix { };
 			};
 		};
-
-		packages.x86_64-linux.swapmods = nixpkgs.legacyPackages.x86_64-linux.callPackage ./derivation.nix {};
-		packages.x86_64-linux.default = self.packages.x86_64-linux.swapmods;
-	};
+	});
 }
+
 
